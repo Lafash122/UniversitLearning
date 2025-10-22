1e4b251a1a448ab7d8f42e45630f8ad09ba697bf
Lafash122 <120730181+Lafash122@users.noreply.github.com>
2025-10-22 15:29:23 +0700

    Enable user input for location search
    Added user input for location name in main method instead of in getLocation()

--- Third course/NETS/lab3/Main.java
+++ Third course/NETS/lab3/Main.java
@@ -1,11 +1,20 @@
 import java.util.concurrent.CompletableFuture;
+import java.util.*;
 import java.io.*;
 
 public class Main {
 	public static void main(String[] args) {
 		try {
 			HTTPSearcher s = new HTTPSearcher();
-			s.getLocation().thenCompose(point -> s.getLocationInfo(point)).thenAccept(str -> System.out.println(str)).join();
+
+			Scanner sc = new Scanner(System.in);
+			System.out.println("Enter the location name:");
+			String placeName = sc.nextLine();
+
+			s.getLocation(placeName)
+				.thenCompose(point -> s.getLocationInfo(point))
+				.thenAccept(str -> System.out.println(str))
+				.join();
 		}
 		catch (Exception e) {
 			System.out.println(e.getMessage());
